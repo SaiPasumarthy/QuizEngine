6c09f5d1d797bd6df474ff18ec2a82d0ceb0a59b
saipasumarthy <saipasumarthy009@gmail.com>
2022-05-02 17:03:02 +0530

    Upgraded to Swift 4.1

--- QuizEngine/Result.swift
+++ QuizEngine/Result.swift
@@ -10,10 +10,4 @@ import Foundation
 public struct Result<Question: Hashable, Answer> {
     public var answers: [Question: Answer]
     public var score: Int
-    
-    public init(answers: [Question: Answer], score: Int) {
-        self.answers = answers
-        self.score = score
-    }
-    
 }
