7a648efd411cc7605e15d7e1459cc25e53cd20a5
saipasumarthy <saipasumarthy009@gmail.com>
2022-05-04 19:30:40 +0530

    Replaced the Flow router with QuizDelegate

--- QuizEngine/Game.swift
+++ QuizEngine/Game.swift
@@ -8,18 +8,37 @@
 import Foundation
 
 @available(*, deprecated)
-public class Game<Question, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
-    let flow: Flow<R>
-    init(flow: Flow<R>) {
+public class Game<Question, Answer, R: Router> {
+    let flow: AnyObject
+    init(flow: AnyObject) {
         self.flow = flow
     }
 }
 
 @available(*, deprecated)
 public func startGame<Question, Answer: Equatable, R: Router>(questions:[Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
-    let flow = Flow(questions: questions, router: router, scoring: { scoring(answers: $0, correctAnswers: correctAnswers)})
+    let flow = Flow(questions: questions, router: QuizDelegateToRouterAdapter(router), scoring: { scoring(answers: $0, correctAnswers: correctAnswers)})
     flow.start()
     return Game(flow: flow)
+}
+
+@available(*, deprecated)
+private class QuizDelegateToRouterAdapter<R: Router> : QuizDelegate {
+    
+    private var router: R
+    
+    init(_ router: R) {
+        self.router = router
+    }
+    
+    func handle(question: R.Question, answerCallback: @escaping (R.Answer) -> Void) {
+        router.routeTo(question: question, answerCallback: answerCallback)
+    }
+    
+    func handle(result: Result<R.Question, R.Answer>) {
+        router.routeTo(result: result)
+    }
+    
 }
 
 private func scoring<Question, Answer: Equatable>(answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
