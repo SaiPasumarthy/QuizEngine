eaf7d93681881be69249836e547a88e356a9ac0e
saipasumarthy <saipasumarthy009@gmail.com>
2022-05-06 16:52:03 +0530

    Refactored the score test code

--- QuizEngineTests/ScoreTest.swift
+++ QuizEngineTests/ScoreTest.swift
@@ -15,46 +15,42 @@ class ScoreTest: XCTestCase {
         XCTAssertEqual(score, 0)
     }
     
-    func test_oneWrongAnswer_scoresZero() {
-        let score = BasicScore.score(for: ["wrong"], compareTo: ["correct"])
+    func test_oneNonMatchedAnswer_scoresZero() {
+        let score = BasicScore.score(for: ["not a match"], compareTo: ["an answer"])
         XCTAssertEqual(score, 0)
     }
     
     func test_oneCorrectAnswer_scoresOne() {
-        let score = BasicScore.score(for: ["correct"], compareTo: ["correct"])
+        let score = BasicScore.score(for: ["an answer"], compareTo: ["an answer"])
         XCTAssertEqual(score, 1)
     }
     
     func test_oneCorrectAnswerOneWrongAnswer_scoresOne() {
-        let score = BasicScore.score(for: ["correct 1", "wrong"], compareTo: ["correct 1", "correct 2"])
+        let score = BasicScore.score(for: ["an answer", "not a match"], compareTo: ["an answer", "another answer"])
         XCTAssertEqual(score, 1)
     }
     
     func test_twoCorrectAnswer_scoresTwo() {
-        let score = BasicScore.score(for: ["correct 1", "correct 2"], compareTo: ["correct 1", "correct 2"])
+        let score = BasicScore.score(for: ["an answer", "another answer"], compareTo: ["an answer", "another answer"])
         XCTAssertEqual(score, 2)
     }
     
-    func test_unEqualAnswers_scoresTwo() {
-        let score = BasicScore.score(for: ["correct 1", "correct 2", "another correct answer"], compareTo: ["correct 1", "correct 2"])
+    func test_withTooManyAnswers_twoMatchingAnswers_scoresTwo() {
+        let score = BasicScore.score(for: ["an answer", "another answer", "an extra answer"], compareTo: ["an answer", "another answer"])
         XCTAssertEqual(score, 2)
+    }
+    
+    func test_withTooManyCorrectAnswers_oneMatchingAnswer_scoresOne() {
+        let score = BasicScore.score(for: ["not matching", "another answer"],
+                                     compareTo: ["an answer","another answer", "an extra answer"])
+        XCTAssertEqual(score, 1)
     }
     
     private class BasicScore {
-        
         static func score(for answers: [String], compareTo correctAnswers: [String]) -> Int {
-                     
-            var score = 0
-            
-            for (index, answer) in answers.enumerated() {
-                if index >= correctAnswers.endIndex {
-                    return score
-                }
-                score += answer == correctAnswers[index] ? 1 : 0
+            return zip(answers, correctAnswers).reduce(0) { score, tuple in
+                return score + (tuple.0 == tuple.1 ? 1 : 0)
             }
-            
-            return score
-            
         }
     }
 }
